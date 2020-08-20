7b9723e86bb6d873ea72aa03b11e340ec4b4f5af
Ali Furkan <a.furkan.2003@gmail.com>
2020-08-21 01:37:29 +0300

    Added Rate limit and Connect to DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,40 @@
-import { Module } from "@nestjs/common";
+import { Module, forwardRef } from "@nestjs/common";
 import { PingModule } from "./ping/ping.module";
+import { UserModule } from "./user/user.module";
+import { OAuthModule } from "./oauth/oauth.module";
+import { ApplicationModule } from "./application/app.module";
+import { ConfigModule } from "@nestjs/config";
+import Config from "./config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { RateLimiterModule, RateLimiterGuard } from "nestjs-rate-limit";
+import { APP_GUARD } from "@nestjs/core";
 
 @Module({
-    imports: [PingModule],
+    imports: [
+        RateLimiterModule.forRoot({
+            points: 100,
+            duration: 5,
+            keyPrefix: "global",
+        }),
+        ConfigModule.forRoot({
+            isGlobal: true,
+            load: [Config],
+        }),
+        TypeOrmModule.forRoot({
+            type: "mongodb",
+            url: Config().MONGODB_URI,
+            database: "Picassco",
+            synchronize: true,
+            logger: "debug",
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+            autoLoadEntities: true,
+        }),
+        PingModule,
+        ApplicationModule,
+        UserModule,
+        OAuthModule,
+    ],
+    providers: [{ provide: APP_GUARD, useClass: RateLimiterGuard }],
 })
 export class AppModule {}
