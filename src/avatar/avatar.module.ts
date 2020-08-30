8d3e8fb192eabb608c57ca3cf20ebe54e01f9374
ali-furkqn <mail@alifurkan.codes>
2020-08-30 19:58:54 +0300

    Created Avatar Module

--- src/avatar/avatar.module.ts
+++ src/avatar/avatar.module.ts
@@ -1,8 +1,14 @@
 import { Module } from "@nestjs/common";
 import { AvatarController } from "./avatar.controller";
 import { AvatarService } from "./avatar.service";
+import { SnowflakeModule } from "@app/snowflake";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { UserEntity } from "src/user/user.entity";
+import { UserModule } from "src/user/user.module";
+import { OAuthModule } from "src/oauth/oauth.module";
 
 @Module({
+    imports: [TypeOrmModule.forFeature([UserEntity]),UserModule,SnowflakeModule,OAuthModule],
     controllers: [AvatarController],
     providers: [AvatarService],
 })
